79af2084b161afe7f4d652e0aed3c0e35f770b77
Christopher Tan <phertan02@gmail.com>
2024-09-06 01:06:19 +0800

    Part 4

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,59 +1,62 @@
-
+import java.io.IOException;
+import java.util.Collections;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
+/**
+ * Represents a dialog box consisting of an ImageView to represent the speaker's face
+ * and a label containing text from the speaker.
+ */
 public class DialogBox extends HBox {
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
-        displayPicture = new ImageView(i);
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        text.setWrapText(true);
-        text.setPadding(new Insets(10));
-        displayPicture.setFitWidth(100);
-        displayPicture.setFitHeight(100);
-        displayPicture.setClip(new Circle(50, 50, 50));
-        this.setAlignment(Pos.TOP_RIGHT);
-
-
-        // adds the nodes to the children list
-        this.getChildren().addAll(text, displayPicture);
-        this.setPadding(new Insets(10));
+        dialog.setText(text);
+        Circle circle = new Circle(50,50, 50);
+        displayPicture.setClip(circle);
+        displayPicture.setImage(img);
     }
 
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
+        Collections.reverse(tmp);
         this.getChildren().setAll(tmp);
+        dialog.getStyleClass().add("reply-label");
+        this.setAlignment(Pos.TOP_LEFT);
     }
 
-    public static DialogBox getUserDialog(String s, Image i) {
-        DialogBox db = new DialogBox(s, i);
-        db.setBackground(new Background(new BackgroundFill(
-                Color.PALEGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
+    }
+
+    public static DialogBox getDukeDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
+        db.flip();
         return db;
     }
-
-    public static DialogBox getBotDialog(String s, Image i) {
-        DialogBox dialogBox = new DialogBox(s, i);
-        dialogBox.flip();
-        return dialogBox;
-    }
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,94 +1,38 @@
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+/**
+ * A GUI for Duke using FXML.
+ */
 public class Main extends Application {
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Duke bot = new Duke();
+
+    private Duke duke = new Duke();
 
     @Override
     public void start(Stage stage) {
-        //Setting up required components
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
 
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-        // scroll to the end every time height changes
-        dialogContainer.heightProperty().addListener(observable -> scrollPane.setVvalue(1.0));
+            // Set the root node
+            AnchorPane rootNode = new AnchorPane();
+            fxmlLoader.setRoot(rootNode);
 
-        userInput = new TextField();
-        sendButton = new Button("Send");
+            AnchorPane root = fxmlLoader.load();
+            fxmlLoader.<MainWindow>getController().setDuke(duke);  // inject the Duke instance
 
-        //Formatting the window to look as expected
 
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-        scene = new Scene(mainLayout);
-        stage.setScene(scene);
-        stage.show();
-
-        // Handle user input
-        sendButton.setOnMouseClicked(event -> {
-            handleUserInput();
-        });
-        userInput.setOnAction(event -> {
-            handleUserInput();
-        });
-
-        //More code to be added here later
-
-    }
-    private void handleUserInput() {
-        String userText = userInput.getText();
-        String botText = bot.getResponse(userText);
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, userImage),
-                DialogBox.getBotDialog(botText, dukeImage)
-        );
-        userInput.clear();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
